6b633da86d4f2ce5ce3087e487ff5902daef32cc
fsochaty <pervakov.2020@inbox.ru>
2026-01-28 10:42:54 +0300

    change func find PATH

--- path-helper/cmd/addpath.go
+++ path-helper/cmd/addpath.go
@@ -6,8 +6,10 @@ package cmd
 import (
 	"fmt"
 	"runtime"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/windows/registry"
 )
 
 // addpathCmd represents the addpath command
@@ -22,10 +24,12 @@ For example, if you downloaded the utility,
 you need to call it anywhere in the system.
 Adding it to the PATH will complete this task.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) >= 1 {
+		if len(args) > 1 {
 			fmt.Println("To run this command, specify the path you want to add to PATH as the only argument.")
+			fmt.Println(args)
 			return
 		}
+		path := args[0]
 
 		_const, _ := cmd.Flags().GetBool("const")
 		if _const {
@@ -33,7 +37,10 @@ Adding it to the PATH will complete this task.`,
 			switch os {
 
 			case "windows":
-				fmt.Println("Запущено на Windows")
+				err := addUserPathInPATH(path)
+				if err != nil {
+					fmt.Println("Error read PATH user:", err)
+				}
 
 			case "linux":
 				fmt.Println("Запущено на Linux")
@@ -51,6 +58,95 @@ Adding it to the PATH will complete this task.`,
 			fmt.Println("The path is temporarily added to PATH 😏")
 		}
 	},
+}
+
+func removeUserPathInPATH(path string) error {
+	key, err := registry.OpenKey(
+		registry.CURRENT_USER,
+		`Environment`,
+		registry.ALL_ACCESS,
+	)
+	if err != nil {
+		return fmt.Errorf("Error registry.OpenKey: %w", err)
+	}
+
+	defer key.Close()
+
+	PATH, _, err := key.GetStringValue("PATH")
+	if err != nil {
+		return fmt.Errorf("Couldn't extract data from Path: %w", err)
+	}
+
+	paths := strings.Split(PATH, ";")
+	lenPaths := len(paths)
+	newPaths := make([]string, 0, lenPaths)
+	for _, p := range paths {
+		if p != path {
+			newPaths = append(newPaths, p)
+			lenPaths--
+		}
+	}
+
+	err = key.SetStringValue("PATH", strings.Join(newPaths, ";"))
+	if err != nil {
+		if lenPaths == 0 {
+			return fmt.Errorf("your path is not in the PATH environment")
+		}
+		return err
+	}
+
+	return nil
+}
+
+// для удаления
+func addUserPathInPATH(newPath string) error {
+	key, err := registry.OpenKey(
+		registry.CURRENT_USER,
+		`Environment`,
+		registry.ALL_ACCESS,
+	)
+	if err != nil {
+		return err
+	}
+	defer key.Close()
+
+	PATH, _, err := key.GetStringValue("PATH")
+	if err != nil {
+		return err
+	}
+
+	paths := strings.Split(PATH, ";")
+	for _, p := range paths {
+		if p == newPath {
+			return fmt.Errorf("the path has already been added")
+		}
+	}
+	paths = append(paths, newPath)
+	newPATH := strings.Join(paths, ";")
+
+	return key.SetStringValue("PATH", newPATH)
+}
+
+func LookPATHenvirenment() error {
+	key, err := registry.OpenKey(
+		registry.CURRENT_USER,
+		`Envirenment`,
+		registry.ALL_ACCESS,
+	)
+	if err != nil {
+		return err
+	}
+
+	defer key.Close()
+
+	PATH, _, err := key.GetStringValue("PATH")
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("PATH = %s", PATH)
+
+	return nil
 }
 
 func init() {
